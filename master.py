5d0a5d4fb3d8c59e995049945da09f9bb8f5d254
Nikhil Solanki <nikhil.u.solanki@gmail.com>
2022-04-01 13:09:26 -0400

    Master Theorem
    The Master Theorem proved to be a very important theorem in computer science and theoretical mathematics. It helps one analyze algorithms and functions that rely on recursion/iterations. The Master Theorem would be useful to programmers since the Master Theorem helps portray the runtimes of algorithms. The runtime of an algorithm is important to a programmer, since the best, most efficient algorithm is always the primary goal when implementing one.

---
+++ master.py
@@ -0,0 +1,39 @@
+# Nikhil Solanki
+# CS 260
+# HW 09
+# Master Theorem
+
+import math
+
+def welcome():
+    print("Automated Master Theorem\n")
+    print("Enter Formula T(n)=a*T(n/b)+n^x*(log2(n)^y)\n")
+welcome()
+
+def var():
+    # Inputs from user:
+    global a, b, x, y, n, c
+    a = int(input("Enter a value: "))
+    b = int(input("Enter b value: "))
+    x = int(input("Enter x value: "))
+    y = int(input("Enter y value: "))
+    c = math.log(a, b) # Calculate the c-values
+var()
+
+def master():
+    
+    if c > x:
+        #Case 1
+        print("T(n) = Theta" + "(" + "n^" + str(int(c)) + ")")
+
+    elif c == x:
+        #Case 2
+        print("T(n) = Theta" + "(" + "n^" + str(int(c)) + ")" + "log2(n)^" + str(int(y + 1)))
+
+    elif c < x:
+        #Case 3
+        # print("T(n) = Theta" + "(" + "Ω(n^" + str(int(c + 1)) + ")" + ")")
+        print("T(n)=Theta" + "(" + "n^" + str(int(c + 1)) + ")")
+    
+
+master()
